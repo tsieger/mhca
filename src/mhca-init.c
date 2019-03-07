6ba6f3e4c5704d56c049d655f1b243c904d43cdd
tsieger <tomas@sieger>
2019-03-07 17:15:13 +0100

    using R_registerRoutines() and R_useDynamicSymbols() to make the "R CMD check --as-cran" happier

--- src/mhca-init.c
+++ src/mhca-init.c
@@ -2,5 +2,17 @@
 #include <R_ext/Rdynload.h>
 
 void R_init_mhca(DllInfo *info) {
-    R_RegisterCCallable("mhca", "mhclust",  (DL_FUNC) &mhclust_);
+    // This is the easier way described at http://r-pkgs.had.co.nz/src.html (Hadley Wickham):
+    //R_RegisterCCallable("mhca", "mhclust",  (DL_FUNC) &mhclust_);
+    // However, "R CMD check --as-cran" is not happy with it, complaining about
+    // missing calls to: `R_registerRoutines`, `R_useDynamicSymbols`.
+
+    // Therefore, we must go the more complex way:
+    static const R_CallMethodDef callMethods[]  = {
+        {"mhclust_", (DL_FUNC) &mhclust_,10},
+        {NULL, NULL, 0}
+    };
+    R_registerRoutines(info,NULL,callMethods,NULL,NULL);
+    R_useDynamicSymbols(info,FALSE);
+    R_forceSymbols(info,TRUE);
 }
