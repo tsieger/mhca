0eaad2d1ce852bb87394cc5eead6f03e1924db63
tsieger <tomas@sieger>
2019-03-25 15:31:00 +0100

    apriori starts at 1

--- demo/big.R
+++ demo/big.R
@@ -12,7 +12,7 @@ for (i in 1:m) {
     x[(i-1)*n1+(1:n1),1]<-rnorm(n1)/runif(1,.2,5)+runif(1,-15,15)
     x[(i-1)*n1+(1:n1),2]<-rnorm(n1)/runif(1,.2,5)+runif(1,-15,15)
 }
-apriori<-rep(1:m,each=n1)+1
+apriori<-rep(1:m,each=n1)
 
 # MHCA (w/out apriori clusters)
 st<-system.time(mh<-mhclust(x,thresh=1/m))
@@ -21,7 +21,7 @@ st<-system.time(mh<-mhclust(x,thresh=1/m))
 cmh<-cutree(mhg,k=m)
 # MHCA with apriori clusters
 stg<-system.time(mhg<-mhclust(x,thresh=1/m,g=apriori))
-# note the "thresh=1/m" argument: wel-formed clusters are expected to be formed
+# note the "thresh=1/m" argument: well-formed clusters are expected to be formed
 # by `1/m-th of observations, i.e. by `n1' observation
 cmhg<-cutree(mhg,k=m)
 
@@ -29,14 +29,14 @@ cmhg<-cutree(mhg,k=m)
 opar<-par(mfcol=c(2,2))
 # MHCA
 # feature space plot
-plot(x[,1],x[,2],asp=1,col=apriori,main=paste0('Mahalanobis HCA (MHCA) (n=',n1*m,')'))
+plot(x[,1],x[,2],asp=1,col=apriori+1,main=paste0('Mahalanobis HCA (MHCA) (n=',n1*m,')'))
 # dendrogram
 plot(mh,labels=FALSE,main=paste('MHCA took',round(st[3],2),'s'))
 y<-min(mh$height)-diff(range(mh$height))/10
 text(1:(n1*m),y,(1:(n1*m))[mh$order],col=cmh[mh$order],srt=90)
 # MHCA with apriori clusters
 # feature space plot
-plot(x[,1],x[,2],asp=1,col=apriori,main=paste0('MHCA with apriori clusters (n=',n1*m,')'))
+plot(x[,1],x[,2],asp=1,col=apriori+1,main=paste0('MHCA with apriori clusters (n=',n1*m,')'))
 # dendrogram
 plot(mhg,labels=FALSE,main=paste('apriori MHCA took',round(stg[3],2),'s'))
 y<-min(mhg$height)-diff(range(mhg$height))/10
