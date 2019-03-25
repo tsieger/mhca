c87ba22066521e6fdb376655dc9c4bae495c2d7d
tsieger <tomas@sieger>
2019-03-25 09:22:23 +0100

    "apriori" demo improved; "big" demo added

--- demo/apriori.R
+++ demo/apriori.R
@@ -8,19 +8,26 @@ x0n<-100
 x0<-data.frame(x=runif(2*x0n,-1,1),y=runif(2*x0n,-1,1))
 x0<-x0[x0$x^2+x0$y^2<1,]
 x0<-x0[1:min(nrow(x0),x0n),]
-# a horizontal ellipse
+# shift
+a<-1.5
+# scaling
+b<-8
+# left vertical ellipse
 x1<-x0
-x1$x<-x1$x+1.5
-x1$y<-x1$y/10
-# a vertical ellipse
+x1$x<-x1$x/b-a
+x1$y<-x1$y+a/2
+# horizontal ellipse
 x2<-x0
-x2$x<-x2$x/10
-# another vertical  ellipse
+x2$x<-x2$x
+x2$y<-x2$y/b+a/2
+# upper vertical ellipse
 x3<-x0
-x3$x<-x3$x/10
-x3$y<-x3$y-3.5
-# random noise
-x4<-data.frame(x=rnorm(x0n)-1,y=rnorm(x0n)-1)
+x3$x<-x3$x/b
+x3$y<-x3$y+a
+# lower vertical ellipse
+x4<-x0
+x4$x<-x4$x/b
+x4$y<-x4$y-a
 # combine the data
 x<-rbind(x1,x2,x3,x4)
 rownames(x)<-1:nrow(x)
@@ -29,11 +36,11 @@ n<-nrow(x)
 # number of top-most clusters to take
 k<-4
 
-# MHCA
+# MHCA (w/out apriori clusters)
 mh<-mhclust(x,thresh=.2)
 cmh<-cutree(mh,k=k)
 # MHCA with apriori clusters
-mhg<-mhclust(x,thresh=.25,g=rep(c(1:3,0),c(nrow(x1),nrow(x2),nrow(x3),nrow(x4))))
+mhg<-mhclust(x,thresh=.25,g=rep(c(1:4),c(nrow(x1),nrow(x2),nrow(x3),nrow(x4))))
 # note the "thresh=.25" argument: wel-formed clusters are expected to be formed
 # by 25% of observations
 cmhg<-cutree(mhg,k=k)
@@ -49,9 +56,9 @@ y<-min(mh$height)-diff(range(mh$height))/10
 text(1:n,y,(1:n)[mh$order],col=cmh[mh$order],srt=90)
 # MHCA with apriori clusters
 # feature space plots with 4 top clusters
-plot(x[,1],x[,2],asp=1,col=cmhg,main='MHCA with apriori clusters')
+plot(x[,1],x[,2],asp=1,col=cmhg,main='MHCA with 4 apriori clusters')
 # dendrogram
-plot(mhg,labels=FALSE,main='MHCA with apriori clusters')
+plot(mhg,labels=FALSE,main='MHCA with 4 apriori clusters')
 y<-min(mhg$height)-diff(range(mhg$height))/10
 text(1:n,y,(1:n)[mhg$order],col=cmhg[mhg$order],srt=90)
 par(opar)
---
+++ demo/big.R
@@ -0,0 +1,44 @@
+## Mahalanobis HCA with "big" data (n1=2000) split / not split into apriori clusters.
+## Grouping observations in apriori clusters can speed up MHCA considerably.
+
+# prepare demo data
+set.seed(1)
+# number of samples in each apriori cluster
+n1<-100
+# number of apriori clusters
+m<-20
+x<-matrix(NA,n1*m,2)
+for (i in 1:m) {
+    x[(i-1)*n1+(1:n1),1]<-rnorm(n1)/runif(1,.2,5)+runif(1,-15,15)
+    x[(i-1)*n1+(1:n1),2]<-rnorm(n1)/runif(1,.2,5)+runif(1,-15,15)
+}
+apriori<-rep(1:m,each=n1)+1
+
+# MHCA (w/out apriori clusters)
+st<-system.time(mh<-mhclust(x,thresh=1/m))
+# note the "thresh=1/m" argument: wel-formed clusters are expected to be formed
+# by `1/m'-th of observations, i.e. by `n1' observation
+cmh<-cutree(mhg,k=m)
+# MHCA with apriori clusters
+stg<-system.time(mhg<-mhclust(x,thresh=1/m,g=apriori))
+# note the "thresh=1/m" argument: wel-formed clusters are expected to be formed
+# by `1/m-th of observations, i.e. by `n1' observation
+cmhg<-cutree(mhg,k=m)
+
+# make plots
+opar<-par(mfcol=c(2,2))
+# MHCA
+# feature space plot
+plot(x[,1],x[,2],asp=1,col=apriori,main=paste0('Mahalanobis HCA (MHCA) (n=',n1*m,')'))
+# dendrogram
+plot(mh,labels=FALSE,main=paste('MHCA took',round(st[3],2),'s'))
+y<-min(mh$height)-diff(range(mh$height))/10
+text(1:(n1*m),y,(1:(n1*m))[mh$order],col=cmh[mh$order],srt=90)
+# MHCA with apriori clusters
+# feature space plot
+plot(x[,1],x[,2],asp=1,col=apriori,main=paste0('MHCA with apriori clusters (n=',n1*m,')'))
+# dendrogram
+plot(mhg,labels=FALSE,main=paste('apriori MHCA took',round(stg[3],2),'s'))
+y<-min(mhg$height)-diff(range(mhg$height))/10
+text(1:(n1*m),y,(1:(n1*m))[mhg$order],col=cmhg[mhg$order],srt=90)
+par(opar)
