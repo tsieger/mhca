826ea5b3372582c5f027167ae67108958072618b
tsieger <tomas@sieger>
2021-02-09 14:15:32 +0100

    updating (test of #5)

--- tests/testthat/testMhclustThresh.R
+++ tests/testthat/testMhclustThresh.R
@@ -14,9 +14,10 @@ test_that("low thresh", {
     expect_equal(mhclust(cbind(rep(1,6),c(1,1.1,1.3,4,4.5,4.9)),thresh=.1,useR=TRUE)$height,
         c(0.1,0.25,0.4,0.7,3.333333),tolerance=1e-6)
 })
+# test of issue #5:
 test_that("high thresh", {
     expect_equal(mhclust(cbind(rep(1,6),c(1,1.1,1.3,4,4.5,4.9)),thresh=.99)$height,
-        c(0.1,0.25,0.4,0.7,3.25),tolerance=1e-6)
+        c(0.1,0.25,0.4,0.7,3.246823),tolerance=1e-6)
     expect_equal(mhclust(cbind(rep(1,6),c(1,1.1,1.3,4,4.5,4.9)),thresh=.99,useR=TRUE)$height,
-        c(0.1,0.25,0.4,0.7,3.25),tolerance=1e-6)
+        c(0.1,0.25,0.4,0.7,3.246823),tolerance=1e-6)
 })
